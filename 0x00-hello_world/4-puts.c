a55c67cf588393e1a18fae6b1d9d4c72f3dbec24
Chy-baby <chideraikpeama@gmail.com>
2023-06-15 19:56:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+
+o main - Entry point
+ *
+
+o Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
 
